713b379e2b0cd767daa70b1dc346c6ec0558d551
Akash <2300030412@kluniversity.in>
2026-01-11 11:15:33 +0530

    added for loop file

---
+++ Java-codes/ControlStatements/ForLoop.java
@@ -0,0 +1,11 @@
+package Java-codes.ControlStatements;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
