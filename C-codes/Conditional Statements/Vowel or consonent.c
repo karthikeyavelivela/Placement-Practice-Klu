ed9abb766f59a3bdfe1884a526094d52d7e1f288
SujithKolisetty33005 <2300033005@kluniversity.in>
2026-01-13 04:18:16 +0530

    Vowel or consonent added using switch-case

--- C-codes/Conditional Statements/Vowel or consonent.c
+++ C-codes/Conditional Statements/Vowel or consonent.c
@@ -5,12 +5,11 @@ int main()
     char ch;
     scanf(" %c", &ch);
 
-    if ((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
-        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
-            ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-            printf("Vowel");
-        else
-            printf("Consonant");
+    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+        ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        printf("Vowel");
+    else if ((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
+        printf("Consonant");
     else
         printf("Not an alphabet");
 
