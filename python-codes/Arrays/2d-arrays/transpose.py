87077dd3339486e6de6c1874d01a9fd765c87a36
karthikeyeacoder <velivelakarthikeya@gmail.com>
2026-02-05 15:50:51 +0530

    hoii

--- python-codes/Arrays/2d-arrays/transpose.py
+++ python-codes/Arrays/2d-arrays/transpose.py
@@ -1,5 +1,13 @@
 r,c=map(int,input().split())
 mat=[list(map(int,input().split()))for _ in range(r)]
+
+print("Original Matrix:")
+for i in range(r):
+    for j in range(c):
+        print(mat[i][j],end=" ")
+    print()
+
+print("\nTranspose:")
 for j in range(c):
     for i in range(r):
         print(mat[i][j],end=" ")
